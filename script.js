94c9f996067815f1cd096c61236db9f73ea9a53e
NICOLE SIQUEIRA DORTA <54233987898@info.local>
2024-05-22 10:49:23 -0300

    feat: menu de hamburguer JS

---
+++ script.js
@@ -0,0 +1,35 @@
+let menu = document.getElementById("menu")
+let iconeBarras = document.getElementById("icone-barras")
+let iconeX = document.getElementById("icone-x")
+
+function abreFechaMenu() {
+
+    if (menu.classList.contains("menu-fechado")) {
+
+        menu.classList.remove("menu-fechado")
+
+        iconeBarras.style.display = "none"
+
+        iconeX.style.display = "inline"
+
+    }
+    else {
+        menu.classList.add("menu-fechado")
+
+
+        iconeX.style.display = "none"
+
+
+        iconeBarras.style.display = "inline"
+
+    }
+
+}
+
+onresize = () => {
+    menu.classList.remove("menu-fechado")
+
+    iconeBarras.style.display = "none"
+
+    iconeX.style.display = "inline"
+}
